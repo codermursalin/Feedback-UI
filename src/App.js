2d1481d24e429747835c567e0cabd969c8081c7b
Mursalin Sk <codermursalin>
2022-05-02 08:52:20 +0530

    some bug fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import FeadBackList from "./components/FeedBackList";
 import Header from "./components/Header";
 import FeedbackStats from "./components/FeedbackStats";
